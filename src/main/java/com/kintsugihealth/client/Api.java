189f295a7eba2f411e3ed303f0c4c5acea24f522
Jackson Cassimiro <jackson.c@kintsugihealth.com>
2024-05-17 01:57:45 -0300

    Initial commit.

---
+++ src/main/java/com/kintsugihealth/client/Api.java
@@ -0,0 +1,41 @@
+package com.kintsugihealth.client;
+
+import com.kintsugihealth.client.openapi.PredictionHandler;
+import com.kintsugihealth.client.openapi.api.InitiateApi;
+import com.kintsugihealth.client.openapi.invoker.ApiException;
+import com.kintsugihealth.client.openapi.model.InitiateNewInitiateRequest;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Api {
+    private String xApiKey;
+
+    private InitiateNewInitiateRequest requestParams;
+    private InitiateApi internalApi;
+
+    public Api(String xApiKey, String url, String userId, boolean isInitiated) {
+        this();
+
+        this.xApiKey = xApiKey;
+
+        internalApi = new InitiateApi();
+        internalApi.getApiClient().setBasePath(url);
+
+        requestParams = new InitiateNewInitiateRequest();
+        requestParams.setUserId(userId);
+        requestParams.setIsInitiated(isInitiated);
+    }
+
+    public String newSession() throws ApiException {
+        return internalApi.initiatePost(xApiKey, requestParams).getSessionId();
+    }
+
+    public String getXApiKey() {
+        return xApiKey;
+    }
+
+    public PredictionHandler prediction() {
+        return new PredictionHandler(this);
+    }
+
+}
---
+++ src/main/java/com/kintsugihealth/client/openapi/PredictionHandler.java
@@ -0,0 +1,36 @@
+package com.kintsugihealth.client.openapi;
+
+import com.kintsugihealth.client.Api;
+import com.kintsugihealth.client.openapi.api.PredictSubmitV1Api;
+import com.kintsugihealth.client.openapi.invoker.ApiException;
+import com.kintsugihealth.client.openapi.model.PredictResponsePredictionInitiated;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.io.File;
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class PredictionHandler {
+
+    private Api api;
+
+    public String predict(File audioFile, Integer allowedSampleRate) throws ApiException {
+        BigDecimal sampleRate = allowedSampleRate != null ? BigDecimal.valueOf(allowedSampleRate) : null;
+        PredictSubmitV1Api predictApi = new PredictSubmitV1Api();
+        PredictResponsePredictionInitiated response = predictApi.predictionPost(
+                api.getXApiKey(), audioFile, api.newSession(), sampleRate
+        );
+
+        return response.getSessionId();
+    }
+
+    public String predict(File audioFile) throws ApiException {
+        return predict(audioFile, null);
+    }
+
+    public void getPredictionBySession(String sessionId) {}
+
+    public void getPredictionsByUser(String userId) {}
+}
